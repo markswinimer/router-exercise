8dc34d020a95d9549d78ac928e8c2e3fe5e97bce
markswinimer <markswinimer@gmail.com>
2019-10-06 21:01:55 -0400

    adds styling/bootstrap

--- src/DogList.js
+++ src/DogList.js
@@ -12,14 +12,18 @@ class DogList extends Component {
   render() {
     return(
       <div>
-          <h1 className="display-1 text-center">Dog List</h1>
+          <h1 className="display-1 text-center my-4">Dog List</h1>
           <div className="container">
             <div className="row">
               {this.props.dogs.map(d => (
                 <div className="Dog col-lg-4 text-center" key={d.name}>
-                    <img src={d.src} alt={d.name}/>
+                    <div className="Dog-container">
+                      <Link className="Dog-Container" to={`/dogs/${d.name}`}>
+                      <img src={d.src} alt={d.name}/>
+                      </Link>
+                    </div>
                     <h3>
-                      <Link to={`/dogs/${d.name}`}>{d.name}</Link>
+                      <Link className="underline" to={`/dogs/${d.name}`}>{d.name}</Link>
                     </h3>
                 </div>
               ))}
